bacd40c6c082f7a29701f75c1f78e8043091018e
swordfish6975 <steven.j.cole@gmail.com>
2018-08-10 22:48:00 +1000

    Update config.py

--- seleniumapis/vanguard/config.py
+++ seleniumapis/vanguard/config.py
@@ -1,4 +1,4 @@
-LOGIN_PATH = "https://investor.vanguard.com/my-account/log-on"
+LOGIN_PATH = "https://secure.vanguardinvestments.com.au/investor"
 HOME_PATH = "https://secure.vanguardinvestments.com.au/vanguard_online/investor/investmentsummary"
 BALANCES_AND_HOLDINGS_PATH = "https://secure.vanguardinvestments.com.au/vanguard_online/investor/investmentsummary"
 COST_BASIS_PATH = "https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/CostBasisSummary.xhtml"
